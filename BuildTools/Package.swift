0b13cacd16222e2d31a17baaab46f81163706777
yanpepik <yan.pepik2001@gmail.com>
2022-03-21 13:48:46 +0300

    Add dependencies

---
+++ BuildTools/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    dependencies: [
+         .package(url: "https://github.com/mac-cain13/R.swift", from: "6.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "BuildTools",
+            dependencies: []),
+    ]
+)
